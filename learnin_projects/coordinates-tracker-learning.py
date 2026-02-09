1a84e67f30d7b9f052b79bdc0ce4f39c2db50ba6
DevK4n3k1 <klander.dejan@gmail.com>
2026-02-09 22:08:05 +0100

    builded a skeleton for corrdinates tracker app

---
+++ learnin_projects/coordinates-tracker-learning.py
@@ -0,0 +1,30 @@
+def main():
+    print("=== Coordinates Tracker ===")
+    coordinates = []
+
+while True:
+    print("\n--- Menu ---")
+    print("1) Add a coordinate (x, y)")
+    print("2) View all coordinates")
+    print("3) Show distance between two points")
+    print("4) quit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        print("Logic to add a coordiante goes here.")
+
+    elif choice == "2":
+        print("Logic to display all cordinates goes here.")
+
+    elif choice == "3":
+        print("Logic to calculate distance between two points goes here.")
+
+    elif choice == "0":
+        print("Goodbye!!!")
+
+    else:
+        print("Invalid choice. Try again.")
+
+if __name__ == "__main__":
+    main()
