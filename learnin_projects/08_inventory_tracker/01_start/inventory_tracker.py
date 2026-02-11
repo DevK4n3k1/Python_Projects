63c03fefe524b7d8a552cbe836f6216b0f14c174
DevK4n3k1 <klander.dejan@gmail.com>
2026-02-11 22:04:07 +0100

    finished 11 02 - started on inventory tracker

---
+++ learnin_projects/08_inventory_tracker/01_start/inventory_tracker.py
@@ -0,0 +1,33 @@
+def main():
+   print("===  Invenotry Tracker ===")
+   inventory = {} # here we will store items and their quantities
+
+   
+   while True:
+       print("")
+       print("")
+       print("")
+       print("")
+       print("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
