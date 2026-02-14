3a05d9f751682cfd89e50676fbb8f073acd5b73b
DevK4n3k1 <klander.dejan@gmail.com>
2026-02-14 20:22:52 +0100

    some small changes to inv tracker

--- learnin_projects/08_inventory_tracker/01_start/inventory_tracker.py
+++ learnin_projects/08_inventory_tracker/01_start/inventory_tracker.py
@@ -9,21 +9,17 @@ def main():
        print("")
        print("")
        print("")
+    
+    choice = input("Choose an option (1-4): ")
+   
+   if choice == "1":
+       print("Add item logic here")
+    
+    elif Choice == "2":
+       print("Remove item logic here")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    else:
+       print("Invalid choice, Please select 1-4.")
 
 
 
