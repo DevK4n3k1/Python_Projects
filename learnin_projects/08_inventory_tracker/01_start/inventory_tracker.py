a2c9f9f7e3147e5331604d6c9c4423321a1d580e
DevK4n3k1 <klander.dejan@gmail.com>
2026-02-23 21:47:48 +0100

    50% done

--- learnin_projects/08_inventory_tracker/01_start/inventory_tracker.py
+++ learnin_projects/08_inventory_tracker/01_start/inventory_tracker.py
@@ -1,80 +1,38 @@
 def main():
-   print("===  Invenotry Tracker ===")
-   inventory = {} # here we will store items and their quantities
-
-   
-   while True:
-       print("")
-       print("")
-       print("")
-       print("")
-       print("")
-    
-    choice = input("Choose an option (1-4): ")
-   
-   if choice == "1":
-       print("Add item logic here")
-    
-    elif Choice == "2":
-       print("Remove item logic here")
-
-    else:
-       print("Invalid choice, Please select 1-4.")
-
- while True:
-        print("\nOptions:")
-        print("1. Add item")
-        print("2. Remove item")
-        print("3. View inventory")
-        print("4. Quit")
-
-         while True:
-        print("\nOptions:")
-        print("1. Add item")
-        print("2. Remove item")
-        print("3. View inventory")
-        print("4. Quit")
-
-         while True:
-        print("\nOptions:")
-        print("1. Add item")
-        print("2. Remove item")
-        print("3. View inventory")
-        print("4. Quit")
+    print("=== Inventory Tracker ===")
+    inventory = {}  # this will store items and their quantities
 
     while True:
         print("\nOptions:")
         print("1. Add item")
         print("2. Remove item")
         print("3. View inventory")
-        print("4. Quit") while True:
-        print("\nOptions:")
-        print("1. Add item")
-        print("2. Remove item")
-        print("3. View inventory")
         print("4. Quit")
-   while True:
-       print("")
-       print("")
-       print("")
-       print("")
-       print("")
-    
-    choice = input("Choose an option (1-4): ")
-   
-   if choice == "1":
-       print("Add item logic here")
-    
-    elif Choice == "2":
-       print("Remove item logic here")
 
-    else:
-       print("Invalid choice, Please select 1-4.")
+        choice = input("Choose an option (1-4): ")
 
+        if choice == "1":
+            item = input("Enter itenm name: ").strip().lower()
+            qty =input("Enter quantity: ")
 
+            if not qty.isdigit():
+               print("Quantity must be a number.")   
+               continue
 
+            qty = int(qty)             
 
+        elif choice == "2":
+            print("Remove item logic here")
 
+        elif choice == "3":
+            print("View inventory logic here")
+
+        elif choice == "4":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid choice. Please select 1-4.")
 
 if __name__ == "__main__":
     main()
