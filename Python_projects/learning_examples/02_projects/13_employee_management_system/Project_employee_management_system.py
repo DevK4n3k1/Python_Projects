680f99c8dade23aae53a9bbdfe3eaf6cb9485cc6
DevK4n3k1 <klander.dejan@gmail.com>
2026-05-05 20:34:31 +0200

    fixed a bug

--- Python_projects/learning_examples/02_projects/13_employee_management_system/Project_employee_management_system.py
+++ Python_projects/learning_examples/02_projects/13_employee_management_system/Project_employee_management_system.py
@@ -4,7 +4,3 @@ city = input("What is your city? ")
 
 # Show user
 print(f"Hello, {name}. {city} is a nice city")
-
-print(f"Hello, {name}. {city} is a nice city")
-
-print(f"Hello, {name}. {city} is a nice city")
