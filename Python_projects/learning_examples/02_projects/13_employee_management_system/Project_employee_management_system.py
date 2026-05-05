38a288cee01c8bbbf607377021b24715c6d0d919
DevK4n3k1 <klander.dejan@gmail.com>
2026-05-05 20:34:06 +0200

    started new projec

--- Python_projects/learning_examples/02_projects/13_employee_management_system/Project_employee_management_system.py
+++ Python_projects/learning_examples/02_projects/13_employee_management_system/Project_employee_management_system.py
@@ -4,3 +4,7 @@ city = input("What is your city? ")
 
 # Show user
 print(f"Hello, {name}. {city} is a nice city")
+
+print(f"Hello, {name}. {city} is a nice city")
+
+print(f"Hello, {name}. {city} is a nice city")
